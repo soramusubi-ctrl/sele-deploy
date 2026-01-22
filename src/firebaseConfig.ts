b1e30dabeb5c7fcc4a913ed6211faa2a43c12aa0
soramusubi-ctrl <minozenkonan@gmail.com>
2026-01-22 13:59:47 +0900

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,7 +3,7 @@ import { getAuth, Auth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "", // 環境変数から取得、なければ空文字
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || ""AIzaSyCvJd01GQ-x1vHRNBxYYeF0GjFbODlIgSQ"", 
   authDomain: "soraai-858d3.firebaseapp.com",
   projectId: "soraai-858d3",
   storageBucket: "soraai-858d3.appspot.com",
